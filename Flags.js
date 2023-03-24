3bd48db61ecf0f9a11758d88c89ef34b3123fa9e
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2023-03-23 23:03:59 -0300

    Adiciona o arquivo Flags.js

---
+++ Flags.js
@@ -0,0 +1,7 @@
+// g - global
+// i = ignore case
+
+const texto = 'Calos assinou o abaixo-assinado'
+console.log(texto.match(/C|ab/)) // A expressão regular fica entre os / /
+console.log(texto.match(/c|ab/i)) // As flags ficam depois do / /
+console.log(texto.match(/ab|c/gi))
