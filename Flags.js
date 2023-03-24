93d0c52587c7609290c5b2d1b96a48e5fc5a2a4b
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2023-03-23 23:06:01 -0300

    Atualiza o arquivo Flag.js

--- Flags.js
+++ Flags.js
@@ -1,7 +1,7 @@
 // g - global
 // i = ignore case
 
-const texto = 'Calos assinou o abaixo-assinado'
+const texto = 'Carlos assinou o abaixo-assinado'
 console.log(texto.match(/C|ab/)) // A expressão regular fica entre os / /
 console.log(texto.match(/c|ab/i)) // As flags ficam depois do / /
 console.log(texto.match(/ab|c/gi))
